28430352128af207cfe8c121bc9668b3261c2fbd
Alejandro Andres Masciotra <51864263+amasciot@users.noreply.github.com>
2024-09-07 12:58:39 -0300

    Ejercicio 13

---
+++ Ejercicio 13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Timers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Se solicitan parametros de entrada 
+
+
+        Console.WriteLine("Indique dos numeros enteros a continuacion :");
+        Console.WriteLine("tipee el primer número");
+        int primero = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("tipee el segundo número");
+        int segundo = Int32.Parse( Console.ReadLine());
+
+        // Se realizan las verfificaciones para responder a la consigna 
+
+        if( primero > 0 && segundo >0)
+        {
+            Console.WriteLine("ambos numeros son positivos");
+        }
+        if (primero < 0 && segundo < 0)
+        {
+            Console.WriteLine("ambos numeros son negativos");
+        }
+        if ( (primero * segundo) < 0)
+        {
+            Console.WriteLine("uno de los dos es positivo");
+        }
+        if( primero == 0 || segundo == 0 ) 
+        {
+            Console.WriteLine("alguno de los numeros es 0");
+        }
+    }
+}
