9f8637ba2bda015c93449a753af24187a2c74485
Alejandro Andres Masciotra <51864263+amasciot@users.noreply.github.com>
2024-09-07 12:56:27 -0300

    Ejemplo 4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // se solicitan los datos de entrada 
+        Console.WriteLine("Introduce un número entero distinto de cero ");
+        int numero = Int32.Parse(Console.ReadLine()); 
+
+        // Se coloca una titulo para dar mejor forma a la respuesta.
+        Console.WriteLine($"El número {numero} tiene los siguientes divisores :");
+
+        // recorremos los posibles divisiores del numero 
+        for (int i = 0; i < numero; i++)
+        {
+
+            if (numero % (i + 1) == 0)
+            {
+                Console.WriteLine($"Es divisible por {i + 1}"); //Se muestran solo los divisores.
+            }
+        }
+
+    }
+}
